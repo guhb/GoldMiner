ee1c4f3503f551f1b054f3158c251644185fda5f
Koh Ou Ping <kohouping@gmail.com>
2012-07-16 20:52:02 +0800

    Add placeholder files copied from the MoonWorriors project

---
+++ Classes/Config/global.js
@@ -0,0 +1,14 @@
+var winSize = null;
+var keys = [];
+var global = {
+    mode: {
+        easy: 0,
+	normal: 1,
+	hard: 2,
+	challenge: 3
+    },
+    level:1,
+    life:4,
+    score:0,
+    sound:true,
+};
